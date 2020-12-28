4515196541f88ee2b66f5952f5e068adaf88a50a
Wang <wangyi7@oregonstate.edu>
2020-12-28 14:49:55 -0800

    mv.bat

--- 2020/12/27/mv.bat
+++ 2020/12/27/mv.bat
@@ -1,10 +1,10 @@
-
-del tmp\*.jpg
+del 	*.mp4
+del 	tmp\*.jpg
 
 
 for /l %%x in (l, 1, %1) do (
-        copy p0\p.jpg tmp\%%x.jpg
+        copy p0\p.xd tmp\%%x.jpg
 )
 
 
-ffmpeg -framerate 1 -i tmp\%%d.jpg -i voa20201227.mp3 -r 1 -s 1920x1080 -vf subtitles=voa20201227.srt -y mp4\vs1.mp4  
+ffmpeg -framerate 1 -i tmp\%%d.jpg -i voa20201227.mp3 -r 1 -s 1920x1080 -vf subtitles=voa20201227.srt -y v.mp4  
